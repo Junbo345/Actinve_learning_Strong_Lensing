63b159d705fe342abe1b85fadfd7f5b203eb8f3b
Junbo Li <junb.li@mail.utoronto.ca>
2024-11-12 21:18:35 -0500

    Updated data downloading

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -14,10 +14,13 @@ library(tidyverse)
 #### Download data ####
 data_Mortality <- WDI(indicator = "SH.DYN.MORT", start = 2022, end = 2022)
 data_Food <- WDI(indicator = "AG.PRD.FOOD.XD", start = 2022, end = 2022)
-
+data_DPT <- WDI(indicator = "SH.IMM.IDPT", start = 2022, end = 2022)
+data_Healthexpense <- WDI(indicator = "SH.XPD.CHEX.PC.CD", start = 2021, end = 2021)
 
 #### Save data ####
 write_csv(data_Food, "data/01-raw_data/raw_data_Food.csv") 
-write_csv(data_Mortality, "data/01-raw_data/raw_data_Mortality.csv") 
+write_csv(data_Mortality, "data/01-raw_data/raw_data_Mortality.csv")
+write_csv(data_DPT, "data/01-raw_data/raw_data_DPT.csv")
+write_csv(data_Healthexpense, "data/01-raw_data/raw_data_Healthexpense.csv")
 
          
