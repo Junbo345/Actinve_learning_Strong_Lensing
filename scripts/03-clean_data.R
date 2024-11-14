7e62fc8cf9c9ce01a68faec11712efc3393435f2
Junbo Li <junb.li@mail.utoronto.ca>
2024-11-14 17:16:01 -0500

    cleaned data

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -8,37 +8,34 @@
 # Any other information needed? [...UPDATE THIS...]
 
 #### Workspace setup ####
-library(tidyverse)
+# Load the necessary library
+library(dplyr)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+# List of file paths
+file_paths <- c("file1.csv", "file2.csv", "file3.csv", "file4.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Read and select columns from each file, renaming the unique column
+file1 <- read.csv(data/01-raw_data/raw_data_Food.csv) %>%
+  select(common_column, unique_col1 = AG.PRD.FOOD.XD)
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+file2 <- read.csv(data/01-raw_data/raw_data_Mortality.csv) %>%
+  select(common_column, unique_col2 = SH.DYN.MORT)
+
+file3 <- read.csv(data/01-raw_data/raw_data_DPT.csv) %>%
+  select(common_column, unique_col3 = SH.IMM.IDPT)
+
+file4 <- read.csv(data/01-raw_data/raw_data_Healthexpense.csv) %>%
+  select(common_column, unique_col4 = SH.XPD.CHEX.PC.CD)
+
+# Merge all data frames by 'common_column'
+merged_data <- file1 %>%
+  left_join(file2, by = "country") %>%
+  left_join(file3, by = "country") %>%
+  left_join(file4, by = "country") %>%
+  left_join(file5, by = "country")
+
+# Save the final merged file
+write.csv(merged_data, "data/02-analysis_data/analysis_data.csv", row.names = FALSE)
+
+
+
